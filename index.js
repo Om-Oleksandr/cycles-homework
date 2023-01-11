7d89ddd9b2a7c88dad81d67489cc65db2a46c724
Oleksandr Om <razy.uys.razy00@gmail.com>
2023-01-11 22:21:34 +0200

    Add functions

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-function correctNumber() {
-  const randomNumber = prompt();
-  const correctNumber = Number(randomNumber);
+function isNumberCorrectWhile() {
   const attempts = 3;
-  if (
-    randomNumber === "" ||
-    randomNumber === null ||
-    Number.isNaN(correctNumber)
-  ) {
-    return "error";
-  }
-  while (let(i) < attempts) {
-    i++;
-    if (number >= 15 && number <= 35 && number % 6 === 0) {
-      return number;
+  let iterator = 0;
+  while (iterator < attempts) {
+    iterator++;
+    const randomNumber = prompt("Enter number");
+    const correctNumber = Number(randomNumber);
+    if (correctNumber >= 15 && correctNumber <= 35 && correctNumber % 6 === 0) {
+      return true;
+    }
+    if (
+      randomNumber === "" ||
+      randomNumber === null ||
+      Number.isNaN(correctNumber)
+    ) {
+      return false;
+    }
+    if (iterator === attempts) {
+      return false;
     }
   }
 }
+
+function isNumberCorrectFor() {
+  const attempts = 3;
+  for (let i = 0; i < attempts; i++) {
+    const randomNumber = prompt("Enter number");
+    const correctNumber = Number(randomNumber);
+    if (correctNumber >= 15 && correctNumber <= 35 && correctNumber % 6 === 0) {
+        return true;
+      }
+      if (
+        randomNumber === "" ||
+        randomNumber === null ||
+        Number.isNaN(correctNumber)
+      ) {
+        return false;
+      }
+      if (i === attempts - 1) {
+        return false;
+      }
+  }
+}
+
+
+// debugger;
+console.log(isNumberCorrectWhile());
+console.log(isNumberCorrectFor());
